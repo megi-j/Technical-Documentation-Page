b3f997ae8940dbe6c1ff81c55383c1eabdfe9e76
megi-j <82594147+megi-j@users.noreply.github.com>
2021-11-05 01:07:43 +0400

    change responsive styles totally

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-document.getElementById('burger').addEventListener('click', function(){
-    let navbar = document.getElementById('navbar');
-    navbar.style.display = 'block';
-    navbar.style.width = '23%';
-    let maindoc = document.getElementById('main-doc');
-    maindoc.style.width = '60%';
-    maindoc.style.marginLeft = '15%';
+// document.getElementById('burger').addEventListener('click', function(){
+//     let navbar = document.getElementById('navbar');
+//     navbar.style.display = 'block';
+//     navbar.style.width = '23%';
+//     let maindoc = document.getElementById('main-doc');
+//     maindoc.style.width = '60%';
+//     maindoc.style.marginLeft = '15%';
 
-})
-document.getElementById('hide-header').addEventListener('click', function(){
-    navbar.style.display = "none";
-})
+// })
+// document.getElementById('hide-header').addEventListener('click', function(){
+//     navbar.style.display = "none";
+//     maindoc.style.width = '90%';
+//     maindoc.style.marginLeft = '3%';
+// })
